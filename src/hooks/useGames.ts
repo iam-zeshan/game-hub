dae679ea2a817766770fe800222dece0cfca0798
Zeshan Haider <zeshan.haider@emumba.com>
2023-04-26 11:20:56 -0600

    Show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,22 +24,26 @@ export interface Game {
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setErrors] = useState("");
+    const [isLoading, setLoading] = useState(false);
   
     useEffect(() => {
       const controller = new AbortController();  
       
+      setLoading(true);
       apiClient
         .get<FetchGamesResponse>("/games")
-        .then((res) => setGames(res.data.results))
+        .then((res) => {
+          setGames(res.data.results);
+          setLoading(false);
+        })
         .catch((err) => {
             if (err instanceof CanceledError) return;
             setErrors(err.message);
+            setLoading(false);
         });
-
     return () => controller.abort();
     }, []);
-
-    return { games, error};
+    return { games, error, isLoading};
 }
 
 export default useGames;
