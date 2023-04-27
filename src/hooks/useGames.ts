955f7e21e53dec8323853a0d90875f10adc0d34c
Zeshan Haider <zeshan.haider@emumba.com>
2023-04-27 11:00:48 -0600

    Sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -22,7 +22,8 @@ useData<Game>('/games',
 {
   params: {
     genres: gameQuery.genre?.id,
-    platforms: gameQuery.platform?.id
+    platforms: gameQuery.platform?.id,
+    ordering: gameQuery.sortOrder
   }
 },
 [ gameQuery]);
